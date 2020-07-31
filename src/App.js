918315b9e090ba55159779c42985259b23c8e00a
Steve North <tex0gen@gmail.com>
2020-08-01 00:32:56 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      buttons: "0"
+    }
+  }
+
+  updateScreen() {
+    return this.state.buttons;
+  }
+
+  buttonPress(e) {
+    this.setState({buttons: (this.state.buttons === "0") ? e.target.value:this.state.buttons + e.target.value});
+  }
+
+  calculate() {
+    this.setState({buttons: this.doCalculation()});
+  }
+
+  doCalculation() {
+    return new Function('return ' + this.state.buttons)();
+  }
+
+  clear() {
+    this.setState({buttons: "0"});
+  }
+
+  render() {
+    return (
+      <div className="Calculator">
+        <div className="Calculator--screen">
+          {this.updateScreen()}
+        </div>
+        <div className="Calculator--functions">
+          <div className="Calculator--functions-wrap"><button onClick={(e) => this.clear(e)} value="c">Clear</button></div>
+        </div>
+        <div className="Calculator--buttons">
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="7">7</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="8">8</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="9">9</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="4">4</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="5">5</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="6">6</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="1">1</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="2">2</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="3">3</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value="0">0</button></div>
+          <div className="Calculator--button-wrap"><button onClick={(e) => this.buttonPress(e)} value=".">.</button></div>
+          <div className="Calculator--button-wrap"><button className="Calculator--button-equals" onClick={(e) => this.calculate(e)} value="=">=</button></div>
+        </div>
+        <div className="Calculator--equations">
+          <div className="Calculator--equation-wrap"><button onClick={(e) => this.buttonPress(e)} value="/">÷</button></div>
+          <div className="Calculator--equation-wrap"><button onClick={(e) => this.buttonPress(e)} value="*">x</button></div>
+          <div className="Calculator--equation-wrap"><button onClick={(e) => this.buttonPress(e)} value="-">-</button></div>
+          <div className="Calculator--equation-wrap"><button onClick={(e) => this.buttonPress(e)} value="+">+</button></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
